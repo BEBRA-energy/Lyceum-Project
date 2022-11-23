a47320f5ae69dbcf22b9746bec4fd9f8c60f8faa
PorosenokPoroshenko <fulkach@gmail.com>
2022-11-23 21:22:52 +0300

    добавил vector_helper

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include "Classes/Extractor.h"
+#include "Helpers/VectorHelper.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    Extractor ex("../texts/0.txt");
+    string texts_folder = "../texts";
+    vector<map<string, double>> features;
+    
+    for(int i=1;i<=4;i++){
+        string path = texts_folder + to_string(i); 
+        Extractor extractor(path);
+        features.push_back(extractor.get_all_info());
+    }
 
-    auto res = ex.get_sentences();
-
-    cout << res.size() << '\n';
-
-    for (const auto &el: res)
-        cout << el << '\n';
+     
 
     return 0;
 }
