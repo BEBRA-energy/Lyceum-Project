abf5e0e1705c95a388119861d1283e3f604b453c
PorosenokPoroshenko <fulkach@gmail.com>
2022-11-25 10:51:12 +0300

    main compare added

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,29 @@ int compare(){
         Extractor extractor(path);
         features.push_back(extractor.get_all_info());
     }
+    
+    vector<vector<double>> values_only;
+    for(map<string, double> text_features: features){
+        vector<double> text_values; 
+        for(auto value: text_features)
+            text_values.push_back(value.second);
+        values_only.push_back(text_values);
+    }
 
-    // to be continued....
+    int num_texts = values_only.size();
+    ofstream fout("output.txt");
+    //vector<vector<double>> similarities;
+    for(int i =1;i<=num_texts;i++){
+        vector<double> i_vector_similarities;
+        for(int j = 1;j<=num_texts;j++){
+            vector<double> text1 = values_only[i];
+            vector<double> text2 = values_only[j];
+            //i_vector_similarities.push_back(VectorHelper::cosine_similarity(text1, text2));
+            fout<<"text "<<i<<" and "<<j<<VectorHelper::cosine_similarity(text1, text2)<<endl;
+        }
+        //similarities.push_back(i_vector_similarities);
+    } 
+   f 
 }
 
 int main() {
